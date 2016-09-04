72336e606ecec6e1a540cf8707f45e06f331f7c6
daler <dalerr@niddk.nih.gov>
2016-09-04 15:21:25 -0400

    simplify

--- travis-setup.sh
+++ travis-setup.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 set -e
-if [[ $TRAVIS_OS_NAME = "linux" ]]; then tag="Linux-x86_64"; else tag="MacOSX-x86_64"; fi
-curl -O https://repo.continuum.io/miniconda/Miniconda3-latest-${tag}.sh
-sudo bash Miniconda3-latest-${tag}.sh -b -p /anaconda
+curl -O https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
+sudo bash Miniconda3-latest-Linux-x86_64.sh -b -p /anaconda
 sudo chown -R $USER /anaconda
-mkdir -p /anaconda/conda-bld/osx-64 # workaround for bug in current conda
-mkdir -p /anaconda/conda-bld/linux-64 # workaround for bug in current conda
-export PATH=/anaconda/bin:$PATH
 export PATH="/anaconda/bin:$PATH"
+conda install conda-build
 pip install docker-py
