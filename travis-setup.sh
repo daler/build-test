1e371d15e7ef08f365bcf1cfc95b50da6e24f4a6
daler <dalerr@niddk.nih.gov>
2016-09-04 15:35:30 -0400

    simplify

--- travis-setup.sh
+++ travis-setup.sh
@@ -3,6 +3,7 @@ set -e
 curl -O https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
 sudo bash Miniconda3-latest-Linux-x86_64.sh -b -p /anaconda
 sudo chown -R $USER /anaconda
+mkdir -p /anaconda/conda-bld/osx-64 # workaround for bug in current conda
+mkdir -p /anaconda/conda-bld/linux-64 # workaround for bug in current conda
 export PATH="/anaconda/bin:$PATH"
-conda install -y conda-build
 pip install docker-py
