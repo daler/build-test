5f563bcf103c012722b3982246fa07190f995395
daler <dalerr@niddk.nih.gov>
2016-09-04 15:24:54 -0400

    simplify

--- travis-setup.sh
+++ travis-setup.sh
@@ -4,5 +4,5 @@ curl -O https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
 sudo bash Miniconda3-latest-Linux-x86_64.sh -b -p /anaconda
 sudo chown -R $USER /anaconda
 export PATH="/anaconda/bin:$PATH"
-conda install conda-build
+conda install -y conda-build
 pip install docker-py
