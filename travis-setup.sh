536d0fd65a5724d019e7e083195174b323db97a7
daler <dalerr@niddk.nih.gov>
2016-09-05 16:35:04 -0400

    new anaconda token

--- travis-setup.sh
+++ travis-setup.sh
@@ -7,6 +7,7 @@ sudo chown -R $USER /anaconda
 mkdir -p /anaconda/conda-bld/osx-64 # workaround for bug in current conda
 mkdir -p /anaconda/conda-bld/linux-64 # workaround for bug in current conda
 export PATH="/anaconda/bin:$PATH"
+
 if [[ $TRAVIS_OS_NAME == "linux" ]]; then
     pip install docker-py
 fi
