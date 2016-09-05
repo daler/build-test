a67864018317166f7aff9c5b9e036deaaa9b3df3
daler <dalerr@niddk.nih.gov>
2016-09-05 19:30:14 -0400

    cleanup

--- travis-post-build.sh
+++ travis-post-build.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
+# ensure we can install it
 conda install -y --channel file:///anaconda/conda-bld --override-channels argh
+
+# delete it from the channel (since this is a test repo after all...)
+anaconda -t $ANACONDA_TOKEN remove -f $CONDA_USER/argh/0.26.1
--- travis-run.sh
+++ travis-run.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 export PATH=/anaconda/bin:$PATH
+
+RECIPE=recipes/argh
+
 if [[ $TRAVIS_OS_NAME = "linux" ]]
 then
-    python conda_build_with_docker.py recipes/argh --host-conda-bld /anaconda/conda-bld
+    python conda_build_with_docker.py $RECIPE --host-conda-bld /anaconda/conda-bld
 else
-    conda build recipes/argh
+    conda build $RECIPE
 fi
 
-anaconda -t $ANACONDA_TOKEN upload argh
+anaconda -t $ANACONDA_TOKEN upload -u $CONDA_USER $(conda build --output $RECIPE)
